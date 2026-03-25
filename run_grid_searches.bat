010f0043e8c4e2127760e52015201ca46e5b112d
Timothy Flavin <BinaryOwlizard@gmail.com>
2026-03-25 12:10:15 -0500

    before merge

--- run_grid_searches.bat
+++ run_grid_searches.bat
@@ -7,16 +7,19 @@ if "%~1"=="" (
 
 set COMPUTER_NAME=%~1
 
-echo Running grid search for cartpole...
-python benchmark_dqn.py --grid_search --device_name "%COMPUTER_NAME%" --env_name cartpole
-python benchmark_sac.py --grid_search --device_name "%COMPUTER_NAME%" --env_name cartpole
+echo "Running grid search for cartpole..."
+python benchmark_dqn.py --grid_search --device_name "$COMPUTER_NAME" --env_name cartpole
+python benchmark_pg.py --grid_search --device_name "$COMPUTER_NAME" --env_name cartpole
+python benchmark_sac.py --grid_search --device_name "$COMPUTER_NAME" --env_name cartpole
 
-echo Running grid search for minigrid...
-python benchmark_dqn.py --grid_search --device_name "%COMPUTER_NAME%" --env_name minigrid
-python benchmark_sac.py --grid_search --device_name "%COMPUTER_NAME%" --env_name minigrid
+echo "Running grid search for minigrid..."
+python benchmark_dqn.py --grid_search --device_name "$COMPUTER_NAME" --env_name minigrid
+python benchmark_pg.py --grid_search --device_name "$COMPUTER_NAME" --env_name minigrid
+python benchmark_sac.py --grid_search --device_name "$COMPUTER_NAME" --env_name minigrid
 
-echo Running grid search for mujoco...
-python benchmark_dqn.py --grid_search --device_name "%COMPUTER_NAME%" --env_name mujoco
-python benchmark_sac.py --grid_search --device_name "%COMPUTER_NAME%" --env_name mujoco
+echo "Running grid search for mujoco..."
+python benchmark_dqn.py --grid_search --device_name "$COMPUTER_NAME" --env_name mujoco
+python benchmark_pg.py --grid_search --device_name "$COMPUTER_NAME" --env_name mujoco
+python benchmark_sac.py --grid_search --device_name "$COMPUTER_NAME" --env_name mujoco
 
-echo All grid searches completed!
+echo "All grid searches completed!"
