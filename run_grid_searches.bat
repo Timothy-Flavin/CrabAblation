645b107490a1e5e99fc0500b7e27e2936bdb288d
Timothy Flavin <BinaryOwlizard@gmail.com>
2026-03-25 02:30:36 -0500

    bat

--- run_grid_searches.bat
+++ run_grid_searches.bat
@@ -9,14 +9,14 @@ set COMPUTER_NAME=%~1
 
 echo Running grid search for cartpole...
 python benchmark_dqn.py --grid_search --device_name "%COMPUTER_NAME%" --env_name cartpole
-python benchmark_pg.py --grid_search --device_name "%COMPUTER_NAME%" --env_name cartpole
+python benchmark_sac.py --grid_search --device_name "%COMPUTER_NAME%" --env_name cartpole
 
 echo Running grid search for minigrid...
 python benchmark_dqn.py --grid_search --device_name "%COMPUTER_NAME%" --env_name minigrid
-python benchmark_pg.py --grid_search --device_name "%COMPUTER_NAME%" --env_name minigrid
+python benchmark_sac.py --grid_search --device_name "%COMPUTER_NAME%" --env_name minigrid
 
 echo Running grid search for mujoco...
 python benchmark_dqn.py --grid_search --device_name "%COMPUTER_NAME%" --env_name mujoco
-python benchmark_pg.py --grid_search --device_name "%COMPUTER_NAME%" --env_name mujoco
+python benchmark_sac.py --grid_search --device_name "%COMPUTER_NAME%" --env_name mujoco
 
 echo All grid searches completed!
